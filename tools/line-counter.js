53fc22a1959e23377a5a60ebb60af88cced02e0d
Jeffrey Mesa <JeffreyArt@hotmail.com>
2021-03-21 18:17:20 -0400

    [ADD] comment

--- tools/line-counter.js
+++ tools/line-counter.js
@@ -1,7 +1,8 @@
 const { exec } = require('child_process');
 
+// This util counts the ammount of lines in a document.
 module.exports = {
-  getFileLines: async (filePath) => {
+  countLines: async (filePath) => {
     return new Promise((resolve, reject) => {
       exec(`wc -l ${filePath}`, (err, stdout, stderr) => {
         if (stderr || err) reject(stderr.toString() || err);
