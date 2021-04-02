1752a71ae1a64e5e478a1c42d1d4daac60805572
Jeffrey Mesa <JeffreyArt@hotmail.com>
2021-04-01 22:35:05 -0400

    [ADD] hashmaker

--- tools/hash-maker.js
+++ tools/hash-maker.js
@@ -1,1 +1,9 @@
 // a tool that makes hashes
+const hashMaker = (length) => {
+  length = length <= 0 ? 1 : length;
+  const generate = () =>
+    parseInt(Math.random().toString().substring(2, this.length)).toString(16);
+  let a = '';
+  while (a.length < length) a += generate();
+  return a.slice(0, length);
+};
